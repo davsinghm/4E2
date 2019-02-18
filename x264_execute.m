70fe3299a3c917fb81728fe1a474ee9e006d5a8a
Davinder Singh <davsinghm@gmail.com>
2019-02-18 02:00:52 +0000

    x264_extract: use sprintf for command building

--- x264_execute.m
+++ x264_execute.m
@@ -1,11 +1,8 @@
 function x264_execute(input_file, output_file, crf, ref_frames, sub_me, key_int)
-    x264_opts = strcat("no-psy=1:aq-mode=0:ref=", num2str(ref_frames), ":subme=", num2str(sub_me), ":keyint=", num2str(key_int));
-    % only keyint x264_opts = strcat("keyint=", num2str(keyint));
-    ret_code = system(strcat("cd FFmpeg ", ...
-                    "&& ./ffmpeg -y -i ", "../", input_file, " ", ...
-                    "-c:v libx264 -crf ", num2str(crf)," -x264opts ", x264_opts, " ", ...
-                    "../", output_file));
+    x264_opts = sprintf("no-psy=1:aq-mode=0:ref=%d:subme=%d:keyint=%d", ref_frames, sub_me, key_int);
+    % only keyint x264_opts = sprintf("keyint=%d", keyint);
+    ret_code = system(sprintf("./FFmpeg/ffmpeg -y -i %s -c:v libx264 -crf %d -x264opts %s %s", input_file, crf, x264_opts, output_file));
     if ret_code ~= 0
-        error("x264_execute: exit code is: %d", ret_code);
+        error("ffmpeg exit code is: %d", ret_code);
     end
 end
