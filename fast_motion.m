53f6c8046196d4838e4909a0334bfee04e55f1de
Davinder Singh <davsinghm@gmail.com>
2019-02-25 09:32:03 +0000

    fast_motion.m: fix algorithm
    * return the new mvs as array are passed by values
    * start min_cost by current mv
    * add option to write stats to file
    * simlify candidates matrix

--- fast_motion.m
+++ fast_motion.m
@@ -1,38 +1,49 @@
-function fast_motion(frame, frame_prev, mvs_x, mvs_y, mb_size)
+function [mvs_out_x, mvs_out_y] = fast_motion(frame, frame_prev, mvs_x, mvs_y, mb_size, frame_no)
     mbs_width = size(mvs_x, 2);
     mbs_height = size(mvs_y, 1);
+    write_stats = 0;
+    if write_stats
+        stats_file = fopen(sprintf('fm_stats_frame%03d.txt', frame_no), 'w');
+    end
     % iteration is for skipping the block in current frame
     for iter = 0 : 2
         for mb_x = 2 + iter : 3 : mbs_width - 1
             for mb_y = 2 + iter : 3 : mbs_height - 1
-                min_cost = 1024 * 1024 * 1024; %TODO
+                % start with current motion vector
+                min_cost = cost_mad(frame, frame_prev, mvs_x(mb_y, mb_x), mvs_y(mb_y, mb_x), mb_x, mb_y, mb_size);
 
                 % prepare candidate mvs
-                cand_mv_x = zeros(3, 3);
-                cand_mv_y = zeros(3, 3);
+                candidates = 1;
+                cand_mv_x = zeros(1, 9);
+                cand_mv_y = zeros(1, 9);
                 for cx = -1 : 1
                     for cy = -1 : 1
-                        cand_mv_x(2 + cy, 2 + cx) = mvs_x(mb_y + cy, mb_x + cx);
-                        cand_mv_y(2 + cy, 2 + cx) = mvs_y(mb_y + cy, mb_x + cx);
+                        cand_mv_x(candidates) = mvs_x(mb_y + cy, mb_x + cx);
+                        cand_mv_y(candidates) = mvs_y(mb_y + cy, mb_x + cx);
+                        candidates = candidates + 1;
                     end
                 end
 
                 % try new candidates
-                for cx = 1 : 3
-                    for cy = 1 : 3
-                        mvc_x = cand_mv_x(cx);
-                        mvc_y = cand_mv_y(cy);
-                        cost = cost_mad(frame, frame_prev, mvs_x, mvs_y, mb_x, mb_y, mb_size);
-                        if min_cost < cost
-                            mvs_x(mb_y, mb_x) = mvc_x;
-                            mvs_y(mb_y, mb_x) = mvc_y;
-                            min_cost = cost;
+                for cand = 1 : candidates - 1
+                    cost = cost_mad(frame, frame_prev, cand_mv_x(cand), cand_mv_y(cand), mb_x, mb_y, mb_size);
+                    if cost < min_cost
+                        if write_stats
+                            fprintf(stats_file, 'mb: (%d, %d), min_cost: %d, new_cost: %d, mv: (%d, %d), new_mv: (%d, %d)\n', mb_x, mb_y, min_cost, cost, mvs_x(mb_y, mb_x), mvs_y(mb_y, mb_x), cand_mv_x(cand), cand_mv_y(cand));
                         end
+                        mvs_x(mb_y, mb_x) = cand_mv_x(cand);
+                        mvs_y(mb_y, mb_x) = cand_mv_y(cand);
+                        min_cost = cost;
                     end
                 end
             end
         end
     end
+    if write_stats
+        fclose(stats_file);
+    end
+    mvs_out_x = mvs_x;
+    mvs_out_y = mvs_y;
 end
 
 % calc mad for current func
