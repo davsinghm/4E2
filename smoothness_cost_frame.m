50f669bbeaf82e3613af152b26800d0672394ca0
Davinder Singh <davsinghm@gmail.com>
2019-02-25 12:22:28 +0000

    smoothness_cost_frame: don't include nan values, show warning

--- smoothness_cost_frame.m
+++ smoothness_cost_frame.m
@@ -6,7 +6,12 @@ function cost = smoothness_cost_frame(mvs_x, mvs_y)
     for mb_x = 1 : mbs_width
         for mb_y = 1 : mbs_height
             [neighbors_x, neighbors_y] = get_neighbors(mb_x, mb_y, mvs_x, mvs_y);
-            costs(mb_y, mb_x) = smoothness_cost_mv(mvs_x(mb_y, mb_x), mvs_y(mb_y, mb_x), neighbors_x, neighbors_y);
+            sc = smoothness_cost_mv(mvs_x(mb_y, mb_x), mvs_y(mb_y, mb_x), neighbors_x, neighbors_y);
+            if isnan(sc)
+                sc = 0;
+                fprintf('warning: the smoothness cost is nan for mb: %d, %d\n', mb_x, mb_y);
+            end
+            costs(mb_y, mb_x) = sc;
         end
     end
     cost = mean2(costs);
