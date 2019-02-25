8420d41925bb3d0d8bc73ea330920709e7a26209
Davinder Singh <davsinghm@gmail.com>
2019-02-25 08:47:42 +0000

    fast_motion.m: fix mae cost function

--- fast_motion.m
+++ fast_motion.m
@@ -36,19 +36,27 @@ function fast_motion(frame, frame_prev, mvs_x, mvs_y, mb_size)
 end
 
 % calc mad for current func
-function cost = cost_mad(frame, frame_prev, mvs_x, mvs_y, mb_x, mb_y, mb_size)
-    mv = [mvs_y(mb_y, mb_x), mvs_x(mb_y, mb_x)];
-    mb_diff = zeros(mb_size);
+function cost = cost_mad(frame, frame_prev, mv_x, mv_y, mb_x, mb_y, mb_size)
+    chans = size(frame, 3);
+    % start with nan, so that if loop doesn't run completely, the resulting
+    % mean is nan
+    mb_diff = nan(mb_size(1), mb_size(2), chans);
     start_x = mb_x * mb_size(2);
     start_y = mb_y * mb_size(1);
-    for x1 = 1 : mb_size(2)
-        for y1 = 1 : mb_size(1)
-            curr_x = max(0, min(start_y + y1, size(frame, 2)));
-            curr_y = max(0, min(start_x + x1, size(frame, 1)));
-            prev_x = max(0, min(start_y + y1 + mv(2), size(frame_prev, 2)));
-            prev_y = max(0, min(start_x + x1 + mv(1), size(frame_prev, 1)));
-            mb_diff(y1, x1) = frame(curr_y, curr_x) - frame_prev(prev_y, prev_x);
+    for chan = 1 : chans
+        for x1 = 1 : mb_size(2)
+            for y1 = 1 : mb_size(1)
+                curr_x = clip(floor(start_x + x1), 1, size(frame, 2));
+                curr_y = clip(floor(start_y + y1), 1, size(frame, 1));
+                prev_x = clip(floor(start_x + x1 + mv_x), 1, size(frame_prev, 2));
+                prev_y = clip(floor(start_y + y1 + mv_y), 1, size(frame_prev, 1));
+                mb_diff(y1, x1, chan) = double(frame(curr_y, curr_x, chan)) - double(frame_prev(prev_y, prev_x, chan));
+            end
         end
     end
     cost = mean2(abs(mb_diff));
+end
+
+function ret = clip(val, a, b)
+    ret = max(a, min(val, b));
 end
