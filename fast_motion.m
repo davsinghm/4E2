2f82ab2c50dff86390d5c4dce48f4327bb0590d3
Davinder Singh <davsinghm@gmail.com>
2019-03-05 21:53:19 +0000

    optimize: fast_motion: inline hot cost_mad() function

--- fast_motion.m
+++ fast_motion.m
@@ -1,4 +1,6 @@
 function [mvs_out_x, mvs_out_y] = fast_motion(frame, frame_prev, mvs_x, mvs_y, mb_size, frame_no)
+    frame_width = size(frame, 2);
+    frame_height = size(frame, 1);
     mbs_width = size(mvs_x, 2);
     mbs_height = size(mvs_y, 1);
     write_stats = 0;
@@ -11,12 +13,8 @@ function [mvs_out_x, mvs_out_y] = fast_motion(frame, frame_prev, mvs_x, mvs_y, m
         for jump = 0 : 2
             for mb_x = 2 + jump : 3 : mbs_width - 1
                 for mb_y = 2 + jump : 3 : mbs_height - 1
-                    % start with current motion vector
-                    [neighbors_x, neighbors_y] = get_neighbor_mvs(mb_x, mb_y, mvs_x, mvs_y);
-                    min_cost = cost_mad(frame, frame_prev, mvs_x(mb_y, mb_x), mvs_y(mb_y, mb_x), mb_x, mb_y, mb_size) ...
-                                + smoothness_cost_mv(mvs_x(mb_y, mb_x), mvs_y(mb_y, mb_x), neighbors_x, neighbors_y);
 
-                    % prepare candidate mvs
+                    % prepare candidate mvs, make sure to include current mv
                     candidates = 1;
                     cand_mv_x = zeros(1, 9);
                     cand_mv_y = zeros(1, 9);
@@ -51,11 +49,34 @@ function [mvs_out_x, mvs_out_y] = fast_motion(frame, frame_prev, mvs_x, mvs_y, m
                     end
                 end
 
+                    % frame val positions at current mb
+                    start_x = (mb_x - 1) * mb_size(2) + 1;
+                    start_y = (mb_y - 1) * mb_size(1) + 1;
+                    end_x = start_x + mb_size(2) - 1;
+                    end_y = start_y + mb_size(1) - 1;
+                    block_xs = start_x : end_x;
+                    block_ys = start_y : end_y;
+                    % skip block if out of bounds
+                    if end_x > frame_width || start_x < 1 || end_y > frame_height || start_y < 1
+                        continue;
+                    end
+                    block_curr = frame(block_ys, block_xs);
+                    min_cost = intmax('int64'); % initial
+
                     % test new candidates
                     for cand = 1 : candidates - 1
+                        mv_x = cand_mv_x(cand);
+                        mv_y = cand_mv_y(cand);
+
+                        % skip if out of bounds
+                        if end_x + mv_x > frame_width || start_x + mv_x < 1 || end_y + mv_y > frame_height || start_y + mv_y < 1 ...
+                            || end_x > frame_width || start_x < 1 || end_y > frame_height || start_y < 1
+                            continue;
+                        end
+
                         [neighbors_x, neighbors_y] = get_neighbor_mvs(mb_x, mb_y, mvs_x, mvs_y);
-                        cost = cost_mad(frame, frame_prev, cand_mv_x(cand), cand_mv_y(cand), mb_x, mb_y, mb_size) ...
-                                + smoothness_cost_mv(cand_mv_x(cand), cand_mv_y(cand), neighbors_x, neighbors_y);
+                        cost = mean2(abs(block_curr - frame_prev(block_ys + mv_y, block_xs + mv_x))) ...
+                                + smoothness_cost_mv(mv_x, mv_y, neighbors_x, neighbors_y);
                         if cost < min_cost
                             if write_stats
                                 fprintf(stats_file, 'mb: (%d, %d), min_cost: %d, new_cost: %d, mv: (%d, %d), new_mv: (%d, %d)\n', mb_x, mb_y, min_cost, cost, mvs_x(mb_y, mb_x), mvs_y(mb_y, mb_x), cand_mv_x(cand), cand_mv_y(cand));
@@ -74,30 +95,4 @@ function [mvs_out_x, mvs_out_y] = fast_motion(frame, frame_prev, mvs_x, mvs_y, m
     end
     mvs_out_x = mvs_x;
     mvs_out_y = mvs_y;
-end
-
-% calc mad for current func
-function cost = cost_mad(frame, frame_prev, mv_x, mv_y, mb_x, mb_y, mb_size)
-
-    frame_max_w = size(frame, 2);
-    frame_max_h = size(frame, 1);
-
-    start_x = (mb_x - 1) * mb_size(2) + 1;
-    start_y = (mb_y - 1) * mb_size(1) + 1;
-    end_x = start_x + mb_size(2) - 1;
-    end_y = start_y + mb_size(1) - 1;
-    block_xs = start_x : end_x;
-    block_ys = start_y : end_y;
-
-    % if out of bounds return intmax('int64')
-    if end_x + mv_x > frame_max_w || start_x + mv_x < 1 ...
-        || end_y + mv_y > frame_max_h || start_y + mv_y < 1 ...
-        || end_x > frame_max_w || start_x < 1 ...
-        || end_y > frame_max_h || start_y < 1
-        cost = intmax('int64');
-        return;
-    end
-
-    block_curr = frame(block_ys, block_xs);
-    cost = mean2(abs(block_curr - frame_prev(block_ys + mv_y, block_xs + mv_x)));
 end
