43e8b2531e2c12f22c00760dcaa1c4cd3de9aa4a
Davinder Singh <davsinghm@gmail.com>
2019-03-11 04:44:11 +0000

    main: loop over all seqs returned by get_seqs()

--- main.m
+++ main.m
@@ -15,113 +15,116 @@ end
 
 avg_frames_mc_mad = NaN(1, 51);
 avg_frames_non_mc_mad = NaN(1, 51);
-if 1
+seqs = get_sintel_sequences();
 
-frames_dir = 'alley_1';
-flo_dir = 'tmp/alley_1';
+for seq_i = 1 : size(seqs, 1)
 
-orig_input_file_fmt = sprintf('%s/frame_%%04d.png', frames_dir);
-flo_file_fmt = sprintf('%s/frame_%%04d.flo', flo_dir); % if loading external mvs
+    seq_name = seqs(seq_i, 1);
+    frames_dir = seqs(seq_i, 2);
+    flo_dir = seqs(seq_i, 3);
 
-% generate and read ffmpeg mvs
-if 1
-    temp_mvs_vid_file = "tmp/mvs.mp4"; % temporary file from which the mvs are extracted. the file is encoded from original source by x264, saving mvs to it.
+    orig_input_file_fmt = sprintf('%s/frame_%%04d.png', frames_dir);
+    flo_file_fmt = sprintf('%s/frame_%%04d.flo', flo_dir); % if loading external mvs
 
-    % encode orignal file to intermediary file with specific settings (gop size etc)
-    sub_me = 1; % subme: 7: rd (default), 0: full pel only, 1: qpel sad 1 iter, 2: qpel sad 1 iter
-    bframes_no = 0;
-    ref_frames = 0;
-    key_int = 2; % max interval b/w IDR-frames (aka keyframes)
-    crf = 17
-    x264_execute(orig_input_file_fmt, temp_mvs_vid_file, crf, bframes_no, ref_frames, key_int);
+    % generate and read ffmpeg mvs
+    if 1
+        temp_mvs_vid_file = "tmp/mvs.mp4"; % temporary file from which the mvs are extracted. the file is encoded from original source by x264, saving mvs to it.
 
-    % read mvs from file
-    temp_mvs_file = "tmp/mvs.txt";
-    ffmpeg_export_mvs(temp_mvs_vid_file, temp_mvs_file);
-    [mvs_x, mvs_y, mvs_type, frames_type] = extract_mvs(temp_mvs_file, block_size_w, block_size_h);
-end
+        % encode orignal file to intermediary file with specific settings (gop size etc)
+        sub_me = 1; % subme: 7: rd (default), 0: full pel only, 1: qpel sad 1 iter, 2: qpel sad 1 iter
+        bframes_no = 0;
+        ref_frames = 0;
+        key_int = 2; % max interval b/w IDR-frames (aka keyframes)
+        crf = 17
+        x264_execute(orig_input_file_fmt, temp_mvs_vid_file, crf, bframes_no, ref_frames, key_int);
 
-no_of_frames = size(frames_type, 2);
-% frame data, mad
-frames_mc_mad = NaN(1, no_of_frames);
-frames_non_mc_mad = NaN(1, no_of_frames);
-frames_smoothness_cost = NaN(1, no_of_frames);
-
-for frame_no = 1 : no_of_frames
-    frame = rgb2gray(imread(sprintf(orig_input_file_fmt, frame_no)));
-
-    % ignore other frames for now
-    if frame_no > 1 && frames_type(frame_no) == 'p'
-
-        % fill u and v with same mv from block
-        [height, width, chans] = size(frame);
-        if 1 % ffmpeg mvs
-            frame_mvs_x = mvs_x(:, :, frame_no);
-            frame_mvs_y = mvs_y(:, :, frame_no);
-            if 1 % fast motion: refine motion vectors
-                [frame_mvs_x, frame_mvs_y] = fast_motion(frame, frame_prev, frame_mvs_x, frame_mvs_y, mb_size, frame_no);
-            end
-            frame_flo = fill_dense_mvs_from_blocks([height, width], frame_mvs_x, frame_mvs_y, block_size_w, block_size_h);
-        else % groundtruth
-            frame_flo = -readFlowFile(sprintf(flo_file_fmt, frame_no - 1)); % flow files have negative mvs footnote [1]
-        end
-
-        if 0 % visualize mvs
-            visualize_mvs(frame, 1, frame_flo, 16, 16); % visualize every 16th mv
-        end
-
-        if 0 %save flow color image
-            vis_flow_color = flowToColor(frame_flo);
-            imwrite(vis_flow_color, sprintf('tmp/frame_%04d.png', frame_no));
-        end
-
-        mc_previous = generate_mc_frame(frame_prev, frame_flo);
-
-        mc_mad = mean2(abs(double(frame) - double(mc_previous)));
-        non_mc_mad = mean2(abs(double(frame) - double(frame_prev)));
-        smoothness_cost = smoothness_cost_frame(frame_flo(1), frame_flo(2));
-
-        frames_mc_mad(frame_no) = mc_mad;
-        frames_non_mc_mad(frame_no) = non_mc_mad;
-        frames_smoothness_cost(frame_no) = smoothness_cost;
-        fprintf("frame_no: %03d, mc_diff: %.16f, smoothness: %d\n", frame_no, mc_mad, smoothness_cost);
-
-        % figure(2);
-        % imshow(frame_prev);
-        % title('The previous frame');
-        %
-        % figure(3);
-        % imshow(uint8(mc_previous));
-        % title('The motion compensated previous frame');
-        %
-        % figure(4);
-        % imshow(uint8(128 + double(frame_prev) - double(frame)));
-        % title('The (non-mc) frame difference');
-        %
-        % figure(5);
-        % imshow(uint8(128 + double(frame_prev) - double(mc_previous)));
-        % title('The MC frame difference');
+        % read mvs from file
+        temp_mvs_file = "tmp/mvs.txt";
+        ffmpeg_export_mvs(temp_mvs_vid_file, temp_mvs_file);
+        [mvs_x, mvs_y, mvs_type, frames_type] = extract_mvs(temp_mvs_file, block_size_w, block_size_h);
     end
 
-    frame_prev = frame;
-    frame_no = frame_no + 1;
-end
+    no_of_frames = size(frames_type, 2);
+    % frame data, mad
+    frames_mc_mad = NaN(1, no_of_frames);
+    frames_non_mc_mad = NaN(1, no_of_frames);
+    frames_smoothness_cost = NaN(1, no_of_frames);
 
-% show mad graphs
-figure(1);
-hold on;
-i_y = ~isnan(frames_mc_mad) & ~isnan(frames_non_mc_mad);
-frames_x = 1 : size(i_y(i_y), 2);
-plot(frames_x, frames_mc_mad(i_y));
-plot(frames_x, frames_smoothness_cost(i_y));
-plot(frames_x, frames_non_mc_mad(i_y));
-legend({'MC MAD', 'Smoothness Cost'});
-xlabel('Frame'); ylabel('Cost');
-hold off;
+    for frame_no = 1 : no_of_frames
+        frame = rgb2gray(imread(sprintf(orig_input_file_fmt, frame_no)));
 
-fprintf("avg_mc_diff: %.16f\n", mean(frames_mc_mad(i_y)));
-avg_frames_mc_mad(crf) = mean(frames_mc_mad(i_y));
-avg_frames_non_mc_mad(crf) = mean(frames_non_mc_mad(i_y));
+        % ignore other frames for now
+        if frame_no > 1 && frames_type(frame_no) == 'p'
+
+            % fill u and v with same mv from block
+            [height, width, chans] = size(frame);
+            if 1 % ffmpeg mvs
+                frame_mvs_x = mvs_x(:, :, frame_no);
+                frame_mvs_y = mvs_y(:, :, frame_no);
+                if 0 % fast motion: refine motion vectors
+                    [frame_mvs_x, frame_mvs_y] = fast_motion(frame, frame_prev, frame_mvs_x, frame_mvs_y, mb_size, frame_no);
+                end
+                frame_flo = fill_dense_mvs_from_blocks([height, width], frame_mvs_x, frame_mvs_y, block_size_w, block_size_h);
+            else % groundtruth
+                frame_flo = -readFlowFile(sprintf(flo_file_fmt, frame_no - 1)); % flow files have negative mvs footnote [1]
+            end
+
+            if 0 % visualize mvs
+                visualize_mvs(frame, 1, frame_flo, 16, 16); % visualize every 16th mv
+            end
+
+            if 0 %save flow color image
+                vis_flow_color = flowToColor(frame_flo);
+                imwrite(vis_flow_color, sprintf('tmp/frame_%04d.png', frame_no));
+            end
+
+            mc_previous = generate_mc_frame(frame_prev, frame_flo);
+
+            mc_mad = mean2(abs(double(frame) - double(mc_previous)));
+            non_mc_mad = mean2(abs(double(frame) - double(frame_prev)));
+            smoothness_cost = smoothness_cost_frame(frame_flo(1), frame_flo(2));
+
+            frames_mc_mad(frame_no) = mc_mad;
+            frames_non_mc_mad(frame_no) = non_mc_mad;
+            frames_smoothness_cost(frame_no) = smoothness_cost;
+            fprintf("frame_no: %03d, mc_diff: %.16f, smoothness: %d\n", frame_no, mc_mad, smoothness_cost);
+
+            % figure(2);
+            % imshow(frame_prev);
+            % title('The previous frame');
+            %
+            % figure(3);
+            % imshow(uint8(mc_previous));
+            % title('The motion compensated previous frame');
+            %
+            % figure(4);
+            % imshow(uint8(128 + double(frame_prev) - double(frame)));
+            % title('The (non-mc) frame difference');
+            %
+            % figure(5);
+            % imshow(uint8(128 + double(frame_prev) - double(mc_previous)));
+            % title('The MC frame difference');
+        end
+
+        frame_prev = frame;
+        frame_no = frame_no + 1;
+    end
+
+    % show mad graphs
+    figure(1);
+    hold on;
+    i_y = ~isnan(frames_mc_mad) & ~isnan(frames_non_mc_mad);
+    frames_x = 1 : size(i_y(i_y), 2);
+    plot(frames_x, frames_mc_mad(i_y));
+    plot(frames_x, frames_smoothness_cost(i_y));
+    plot(frames_x, frames_non_mc_mad(i_y));
+    legend({'MC MAD', 'Smoothness Cost'});
+    xlabel('Frame'); ylabel('Cost');
+    hold off;
+
+    fprintf("avg_mc_diff: %.16f\n", mean(frames_mc_mad(i_y)));
+    avg_frames_mc_mad(crf) = mean(frames_mc_mad(i_y));
+    avg_frames_non_mc_mad(crf) = mean(frames_non_mc_mad(i_y));
 
 end
 
