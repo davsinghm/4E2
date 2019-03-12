31e5b1e9f8ee45304e95007e0229efb9b876573e
Davinder Singh <davsinghm@gmail.com>
2019-03-12 04:06:06 +0000

    get neighbors(): use single parameter for mvs

--- get_neighbor_mvs.m
+++ get_neighbor_mvs.m
@@ -1,4 +1,4 @@
-function [neighbors_x, neighbors_y] = get_neighbor_mvs(mb_x, mb_y, mvs_x, mvs_y)
+function [neighbors_x, neighbors_y] = get_neighbor_mvs(mb_x, mb_y, flow)
     neighbors_x = nan(3, 3);
     neighbors_y = nan(3, 3);
     start_x = -1; start_y = -1;
@@ -9,16 +9,16 @@ function [neighbors_x, neighbors_y] = get_neighbor_mvs(mb_x, mb_y, mvs_x, mvs_y)
     if (mb_y == 1)
         start_y = 0;
     end
-    if (mb_x == size(mvs_x, 2))
+    if (mb_x == size(flow, 2))
         end_x = 0;
     end
-    if (mb_y == size(mvs_y, 1))
+    if (mb_y == size(flow, 1))
         end_y = 0;
     end
     for nx = start_x : end_x
         for ny = start_y : end_y
-            neighbors_x(2 + ny, 2 + nx) = mvs_x(mb_y + ny, mb_x + nx);
-            neighbors_y(2 + ny, 2 + nx) = mvs_y(mb_y + ny, mb_x + nx);
+            neighbors_x(2 + ny, 2 + nx) = flow(mb_y + ny, mb_x + nx, 1);
+            neighbors_y(2 + ny, 2 + nx) = flow(mb_y + ny, mb_x + nx, 2);
         end
     end
 end
