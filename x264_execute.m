b8f615f7ce0f68320e9966b6ee0db0693e3781f5
Davinder Singh <davsinghm@gmail.com>
2019-03-13 05:03:21 +0000

    cosmetic: x264 execute: use single qoutes for strings

--- x264_execute.m
+++ x264_execute.m
@@ -1,8 +1,8 @@
 function x264_execute(input_file, output_file, crf, bframes_no, ref_frames, key_int)
     %x264_opts = sprintf("no-psy=1:aq-mode=0:ref=%d:subme=%d:keyint=%d", ref_frames, sub_me, key_int);
-    x264_opts = sprintf("ref=%d:keyint=%d", ref_frames, key_int);
-    ret_code = system(sprintf("./FFmpeg/ffmpeg -y -i %s -c:v libx264 -crf %d -bf %d -x264opts %s %s", input_file, crf, bframes_no, x264_opts, output_file));
+    x264_opts = sprintf('ref=%d:keyint=%d', ref_frames, key_int);
+    ret_code = system(sprintf('./FFmpeg/ffmpeg -y -i %s -c:v libx264 -crf %d -bf %d -x264opts %s %s', input_file, crf, bframes_no, x264_opts, output_file));
     if ret_code ~= 0
-        error("ffmpeg exit code is: %d", ret_code);
+        error('ffmpeg exit code is: %d', ret_code);
     end
 end
