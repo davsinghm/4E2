3bc3f5d95b8c2c88553ebefe27cc51272b339ed0
Davinder Singh <davsinghm@gmail.com>
2019-03-13 04:59:12 +0000

    refactor: get neighbor mvs(): return neighbors as single matrix

--- get_neighbor_mvs.m
+++ get_neighbor_mvs.m
@@ -1,24 +1,23 @@
-function [neighbors_x, neighbors_y] = get_neighbor_mvs(mb_x, mb_y, flow)
-    neighbors_x = nan(3, 3);
-    neighbors_y = nan(3, 3);
+function neighbors = get_neighbor_mvs(x, y, flow)
+    neighbors = nan(3, 3, 2);
     start_x = -1; start_y = -1;
     end_x = 1; end_y = 1;
-    if (mb_x == 1)
+    if (x == 1)
         start_x = 0;
     end
-    if (mb_y == 1)
+    if (y == 1)
         start_y = 0;
     end
-    if (mb_x == size(flow, 2))
+    if (x == size(flow, 2))
         end_x = 0;
     end
-    if (mb_y == size(flow, 1))
+    if (y == size(flow, 1))
         end_y = 0;
     end
     for nx = start_x : end_x
         for ny = start_y : end_y
-            neighbors_x(2 + ny, 2 + nx) = flow(mb_y + ny, mb_x + nx, 1);
-            neighbors_y(2 + ny, 2 + nx) = flow(mb_y + ny, mb_x + nx, 2);
+            neighbors(2 + ny, 2 + nx, 1) = flow(y + ny, x + nx, 1);
+            neighbors(2 + ny, 2 + nx, 2) = flow(y + ny, x + nx, 2);
         end
     end
 end
