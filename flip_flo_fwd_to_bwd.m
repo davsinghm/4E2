62c5ba905a42868e20a97c0f7479b9c931385a6a
Davinder Singh <davsinghm@gmail.com>
2019-03-31 19:31:16 +0100

    cosmetic: rename vars

--- flip_flo_fwd_to_bwd.m
+++ flip_flo_fwd_to_bwd.m
@@ -1,22 +1,22 @@
-function flo = flip_flo_fwd_to_bwd(orig_flo)
-    [height, width, ~] = size(orig_flo);
+function flow = flip_flo_fwd_to_bwd(input_flow)
+    [height, width, ~] = size(input_flow);
 
     x = ones(height, 1) * (1 : width);
     y = (1 : height)' * ones(1, width);
 
-    flo = nan(size(orig_flo));
-    temp_flo(:, :, 1) = round(x + orig_flo(:, :, 1));
-    temp_flo(:, :, 2) = round(y + orig_flo(:, :, 2));
+    flow = nan(size(input_flow));
+    new_pos(:, :, 1) = round(x + input_flow(:, :, 1));
+    new_pos(:, :, 2) = round(y + input_flow(:, :, 2));
 
-    for i = 1 : size(temp_flo, 1);
-        for j = 1 : size(temp_flo, 2);
-            x = temp_flo(i, j, 1);
-            y = temp_flo(i, j, 2);
+    for i = 1 : size(new_pos, 1);
+        for j = 1 : size(new_pos, 2);
+            x = new_pos(i, j, 1);
+            y = new_pos(i, j, 2);
             if x > width || x < 1 || y > height || y < 1
                 continue;
             end;
-            flo(y, x, 1) = -orig_flo(i, j, 1);
-            flo(y, x, 2) = -orig_flo(i, j, 2);
+            flow(y, x, 1) = -input_flow(i, j, 1);
+            flow(y, x, 2) = -input_flow(i, j, 2);
         end
     end
 end
