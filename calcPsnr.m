d66bb0e1f38195594660622fac64048567161a6c
Davinder Singh <davsinghm@gmail.com>
2019-04-11 17:33:35 +0100

    calcPsnr: new function

---
+++ calcPsnr.m
@@ -0,0 +1,10 @@
+function val = psnr(I, Ihat)
+    [rows columns ~] = size(I);
+
+    mseImage = (double(I) - double(Ihat)) .^ 2;
+
+    mse = sum(sum(mseImage)) / (rows * columns);
+
+    % Calculate PSNR (Peak Signal to noise ratio).
+    val = 10 * log10( 255^2 / mse);
+end
